72726748ca8235cca37ad2688131cf2948149b09
박지석 <qkrwltjr0331@gmail.com>
2026-06-29 01:27:42 +0900

    [ADD] 초기 게임 루프 구현을 위한 GameMode, GameState, BalanceData 작성.

--- Source/SpaCh4/Public/Systems/MatchGameMode.h
+++ Source/SpaCh4/Public/Systems/MatchGameMode.h
@@ -1,10 +1,10 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "BalanceData.h"
+#include "MatchGameState.h"
 #include "MatchGameMode.generated.h"
-
-class UBalanceData;
 
 UCLASS()
 class SPACH4_API AMatchGameMode : public AGameModeBase
@@ -14,7 +14,63 @@ class SPACH4_API AMatchGameMode : public AGameModeBase
 public:
 	AMatchGameMode();
 
+	virtual void BeginPlay() override;
+	
+	 
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Flow")
+	void StartMatch();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Flow")
+	void FinishMatch(EMatchResult Result);
+
+	// 아이템 반납, 점수 추가
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Delivery")
+	bool AddDeliveredValue(FName StationId, int32 Value);
+	
+	// 아이템의 가치 확인
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Match|Balance")
+	int32 GetCollectibleValue(ECollectibleType Type) const;
+
+	// 탈출구 개방 조건 확인(또는 GameMode의 OnEscapeGateAvailabilityChanged 이벤트)
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Match|Escape")
+	bool CanActivateEscapeGates() const;
+
+	// 개구멍(개인탈출구) 생성 가능 여부 반환. (또는 GameState의 OnHatchAvailabilityChanged 이벤트)
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Match|Escape")
+	bool CanSpawnHatch() const;
+
+	// 생존자 탈출 등록
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Survivor")
+	void RegisterSurvivorEscaped(FName SurvivorId);
+
+	// 생존자 사망 등록
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Survivor")
+	void RegisterSurvivorKilled(FName SurvivorId);
+
+	// 생존자 상태 변경
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Survivor")
+	void RegisterSurvivorStateChanged(FName SurvivorId, EMatchSurvivorState NewSurvivorState);
+
+	// 결과 확인
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Match|Result")
+	EMatchResult CalculateMatchResult() const;
+
 protected:
+	AMatchGameState* GetMatchGameState() const;
+	const UBalanceData* GetActiveBalanceData() const;
+
+	void InitializeMatchState();
+	void HandleMatchTimerTick();
+	// 탈출구 조건 체크
+	void RefreshEscapeConditions();
+	void TryFinishMatchFromSurvivorCounts();
+	EMatchResult CalculateNoSurvivorLeftResult() const;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Balance")
 	TObjectPtr<UBalanceData> BalanceData;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Match")
+	bool bAutoStartMatch = true;
+
+	FTimerHandle MatchTimerHandle;
 };
