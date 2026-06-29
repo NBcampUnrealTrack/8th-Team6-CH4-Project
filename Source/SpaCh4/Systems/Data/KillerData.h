f23b94c734e799890f20d7f7404dcab0cc88ff54
박지석 <qkrwltjr0331@gmail.com>
2026-06-29 16:20:47 +0900

    [ADD]생존자 살인마용 데이터 에셋 추가

---
+++ Source/SpaCh4/Systems/Data/KillerData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "KillerData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACH4_API UKillerData : public UDataAsset
+{
+	GENERATED_BODY()
+};
