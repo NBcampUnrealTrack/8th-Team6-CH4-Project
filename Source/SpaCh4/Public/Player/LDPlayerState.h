cb20f489a99c140331af8f92464ee26a01f93bda
박지석 <qkrwltjr0331@gmail.com>
2026-07-16 10:06:39 +0900

    [ADD]게임진행 통계 데이터 재정의 및 기록용 함수 추가

--- Source/SpaCh4/Public/Player/LDPlayerState.h
+++ Source/SpaCh4/Public/Player/LDPlayerState.h
@@ -8,65 +8,73 @@
 #include "Systems/LobbyGameState.h"
 #include "LDPlayerState.generated.h"
 
-USTRUCT(BlueprintType)
-struct FSurvivorScore
+UENUM(BlueprintType)
+enum class ESurvivorEscapeMethod : uint8
 {
-	GENERATED_BODY()
-	
-	//납품 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 DeliveryCount = 0;
-	
-	// 납품 점수 획득(소형자주납품 vs 큰거한방?)
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 DeliveryScore = 0;
-	
-	// 부상 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 InjuredCount = 0;
-	
-	// 자신 또는 아군 치유 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 MedkitUseCount = 0;
-	
-	// 감금 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 CagedCount = 0;
-	
-	// 아군 감금 해방 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	int32 CageRescueCount = 0;
-	
-	// 탈출성공
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Score")
-	bool bEscaped = false;
-	
-	
+	None UMETA(DisplayName = "탈출 실패"),
+	Gate UMETA(DisplayName = "탈출구"),
+	Hatch UMETA(DisplayName = "개구멍")
 };
 
 USTRUCT(BlueprintType)
-struct FKillerScore
+struct FSurvivorMatchStats
 {
 	GENERATED_BODY()
 	
-	// 성공적인 공격 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Score")
-	int32 AttackSuccessCount = 0;
+	// 실제 가치가 반영된 납품 완료 횟수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	int32 SuccessfulDeliveryCount = 0;
+
+	// 납품소 목표치에 실제 반영된 가치
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	int32 DeliveredValue = 0;
+
+	// 자가 치료를 완료한 횟수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	int32 SelfHealCount = 0;
+	
+	// 감금 당한 횟수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	int32 CagedCount = 0;
+	
+	// 아군 감금 해방 횟수(구조한 사람이 오름)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	int32 CageRescueCount = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	bool bEscaped = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	bool bKilled = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survivor|Stats")
+	ESurvivorEscapeMethod EscapeMethod = ESurvivorEscapeMethod::None;
+};
+
+USTRUCT(BlueprintType)
+struct FKillerMatchStats
+{
+	GENERATED_BODY()
+	
+	// 성공적인 공격 횟수(피격한 생존자의 상태가 나빠졌을때)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Stats")
+	int32 ValidHitCount = 0;
+
+	// 생존자 다운 성공 횟수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Stats")
+	int32 DownCount = 0;
 	
 	// 생존자 감금 성공 횟수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Score")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Stats")
 	int32 CageCount = 0;
 	
 	// 생존자 처치 카운트
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Score")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Stats")
 	int32 KilledSurvivorCount = 0;
-	
-	//납품 저지 횟수(n범위내에서 생존자의 납품이 취소될때?)
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Killer|Score")
-	int32 InterruptDeliverCount = 0;
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSPPlayerStateLoadoutChangedSignature, const FSPPlayerLoadout&, Loadout);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSPMatchStatsChangedSignature);
 
 /**
  * 
@@ -105,18 +113,62 @@ public:
 	ELobbyPlayerRole PlayerRole = ELobbyPlayerRole::None;
 #pragma endregion
 
+#pragma region GameStats
+	UFUNCTION(BlueprintPure, Category = "Match|Stats")
+	FSurvivorMatchStats GetSurvivorMatchStats() const;
+
+	UFUNCTION(BlueprintPure, Category = "Match|Stats")
+	FKillerMatchStats GetKillerMatchStats() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats")
+	void ResetMatchStats();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordDelivery(int32 AcceptedValue);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordSuccessfulSelfHeal();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordCaged();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordCageRescue();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordEscaped(ESurvivorEscapeMethod EscapeMethod);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Survivor")
+	void RecordKilled();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Killer")
+	void RecordKillerHit(bool bCausedDown);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Killer")
+	void RecordKillerCage();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Match|Stats|Killer")
+	void RecordKillerElimination();
+
+	UPROPERTY(BlueprintAssignable, Category = "Match|Stats")
+	FSPMatchStatsChangedSignature OnMatchStatsChanged;
+#pragma endregion
+
 private:
 	UFUNCTION()
 	void OnRep_PlayerLoadout();
 
+	UFUNCTION()
+	void OnRep_MatchStats();
+
+	void NotifyMatchStatsChanged();
+
 	UPROPERTY(ReplicatedUsing = OnRep_PlayerLoadout, BlueprintReadOnly, Category = "Loadout", meta = (AllowPrivateAccess = true))
 	FSPPlayerLoadout PlayerLoadout;
 	
-	// 생존자 점수정보
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Lobby|Player", meta = (AllowPrivateAccess = true))
-	FSurvivorScore SurvivorScore;
+	UPROPERTY(ReplicatedUsing = OnRep_MatchStats, VisibleAnywhere, BlueprintReadOnly, Category = "Match|Stats|Survivor", meta = (AllowPrivateAccess = true))
+	FSurvivorMatchStats SurvivorStats;
 	
-	// 살인마 점수정보
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Lobby|Player", meta = (AllowPrivateAccess = true))
-	FKillerScore KillerScore;
+	UPROPERTY(ReplicatedUsing = OnRep_MatchStats, VisibleAnywhere, BlueprintReadOnly, Category = "Match|Stats|Killer", meta = (AllowPrivateAccess = true))
+	FKillerMatchStats KillerStats;
 };
